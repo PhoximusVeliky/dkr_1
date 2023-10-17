6d8746d913a68f2388f9a8125893002e6997925f
PhoximusVeliky <144343315+PhoximusVeliky@users.noreply.github.com>
2023-10-17 13:36:33 +0300

    Add files via upload

---
+++ lr №1/1.pas
@@ -0,0 +1,16 @@
+﻿program z1;
+var x,y:real;
+begin
+  Writeln('Введите значение Х');
+  read(x);
+  if x<-8 then y:=(power(x,3)/power(x,3)); //y:=1;
+  if (x>=-8) and (x<-3) then y:=(sin(x)/(sin(x)/cos(x)))*(x/ln(x))
+                        else y:=51;
+  if y=y  then write('x=',x,' ','y=',y)
+          else write('x=',x,' ','y=корней нет');
+end.
+{ power(x,3)/power(x,3) | x<-8
+  ((sin(x)/(sin(x)/cos(x)))*(x/ln(x))) | -8 <= x <-3
+  (sin(x)/cos(x))
+  51 | -3<=x
+  1234567890-1-2-3|-4-5-6-7-8|-9
---
+++ lr №1/12.pas
@@ -0,0 +1,17 @@
+﻿program z2;
+var x,y:real;
+begin
+  x:=-10;
+  while x<=-1 do 
+  begin
+  X:=(Round(X*10))/10; //окургление
+  y:=0;
+  if x<-8 then y:=(power(x,3)/power(x,3)); //y:=1;
+  if (x>=-8) and (x<-3) then y:=(sin(x)/(sin(x)/cos(x)))*(x/ln(x))
+                        else y:=51;
+  if y=y then write('x=',x:1:2,'; ','y=',y)
+         else write('x=',x:1:2,'; ','y=корней нет');
+  x:=x+0.2;
+  writeln;
+  end;
+end.
